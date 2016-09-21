c08b5d373ea0d572004dc1f6e7e23f9883828c8c
Dries Verachtert <dries.verachtert@dries.eu>
2016-09-21 20:14:07 +0200

    also generate lists of base provides for centos 6 and centos 5

--- conflicts-scripts/list-centos-base-provides.sh
+++ conflicts-scripts/list-centos-base-provides.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 
 # repoquery --disablerepo="*" --enablerepo="base" --provides -a | sed 's| .*||g;' > /srv/rpmbuild/centos-base-provides
 
 docker run -ti driesrpms/centos7-x86_64:latest repoquery --disablerepo="*" --enablerepo="base" --provides -a | sed 's| .*||g;' > centos7-x86_64-base-provides
+docker run -ti driesrpms/centos6-x86_64:latest repoquery --disablerepo="*" --enablerepo="base" --provides -a | sed 's| .*||g;' > centos6-x86_64-base-provides
+docker run -ti driesrpms/centos5-x86_64:latest repoquery --disablerepo="*" --enablerepo="base" --provides -a | sed 's| .*||g;' > centos5-x86_64-base-provides
+
 
